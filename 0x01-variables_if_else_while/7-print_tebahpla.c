98b748bacf77ed88ba63e10af51f0a974e27e7e9
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-11 14:36:45 -0700

    hello

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
