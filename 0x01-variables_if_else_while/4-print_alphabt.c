2230b4bbbb4c42191ab57b73d49cfc029de35c91
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-11 14:27:48 -0700

    hello

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'z')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
