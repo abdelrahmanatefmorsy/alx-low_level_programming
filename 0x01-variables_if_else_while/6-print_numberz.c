8b2b6aa813df76ff280313d5d013a8c7834ed15f
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-11 14:33:58 -0700

    hello

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
