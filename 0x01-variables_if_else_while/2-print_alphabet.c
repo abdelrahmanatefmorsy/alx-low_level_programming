3f7e4c13b3a166831465840be3ed90d84d8da365
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-11 14:01:11 -0700

    hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
