9ee7cb2e5e18c08605ae7e4dc527f152ee4324fe
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-19 04:32:24 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * * Description: print putchar
+ *
+ * * Return: 0 (Succsess)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
