f0b825795980525dc3ce254f1738258db7cef6cc
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-19 11:09:10 -0700

    hello

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet- entry point
+ *
+ * * Description: print putchar
+ *
+ * * Return: 0 (Succsess)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
