1083b4ad8f4a86937c240633200e10dfe0686276
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-20 09:31:10 -0700

    hello

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit- entry point
+ *
+ * @n: saged
+ *
+ * * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
