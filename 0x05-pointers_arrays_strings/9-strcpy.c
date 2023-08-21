3e6056473bf1b3a05b3b232d165cdfc3a0579747
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-21 08:14:17 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy- entry point
+ *
+ * @src: saged
+ *
+ * @dest: sagoda
+ *
+ *
+ * * Return: size of @s
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	int i = 0;
+	int j;
+
+	while (src[i])
+		i++;
+	for (j = 0; j < i ; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
