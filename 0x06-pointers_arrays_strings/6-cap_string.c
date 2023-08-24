65b78290d35096e6ee105970227475a177ae376d
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-24 12:21:04 -0700

    hello

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *cap_string- entry point
+ *@str:agoda
+ * * Return: size of @s
+ */
+
+char *cap_string(char *str)
+{
+	int j;
+
+	for (j = 0 ; str[j] != '\0' ; j++)
+	{
+		if (str[j] >= 'a' && str[j] <= 'z' && (str[j - 1] == ',' || str[j - 1] == ';'
+		|| str[j - 1] == '.' || str[j - 1] == '!' || str[j - 1]
+		== '"' || str[j - 1] == '(' || str[j - 1] == ')' || str[j - 1]
+		== '{' || str[j - 1] == '}' || str[j - 1] == ' ' || str[j - 1] ==
+		'\n' || str[j - 1] == '\t'))
+		{
+			str[j] -= 32;
+		}
+	}
+	return (str);
+}
