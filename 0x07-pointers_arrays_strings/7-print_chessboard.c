3c2fb68b75a0f6a97712c23768f40622ec9b0ab7
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-28 12:23:46 -0700

    hello

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_chessboard- entry point
+ *
+ * @a: string that will access
+ *
+ *
+ * * Return: size of @s
+ */
+
+void print_chessboard(char (*a)[8]);
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
