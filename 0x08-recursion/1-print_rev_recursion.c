f5a6687f79cc2d10032dd774cb446c979a483613
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-30 11:52:21 -0700

    hello

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_print_rev_recursion- entry point
+ *
+ * @s: string that will access
+ *
+ *
+ * * Return: size of @s
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
