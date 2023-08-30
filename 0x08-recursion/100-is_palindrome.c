2e645b33161983f77650412b12713c4798554953
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-08-30 15:12:40 -0700

    hello

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,21 +2,50 @@
 #include <stdio.h>
 
 /**
- *_strlen_recursion- entry point
- *
- * @s: string that will access
- *
- *
+ * _square- find sq
+ * @n:number
+ * @i:value
+ * @s:gaded
+ * Return: 1;
+ */
+
+int _square(char *s, int i, int n)
+{
+	if (i == -1)
+	{
+		return (1);
+	}
+	if (s[i] != s[n])
+	{
+		return (0);
+	}
+	return (_square(s, i - 1, n + 1));
+}
+
+/**
+ *index- entry point
+ *@s:pow
  * * Return: size of @s
  */
 
-int _strlen_recursion(char *s)
+int index(char *s)
 {
 	int i = 0;
 
 	if (*s != '\0')
 	{
-		i += _strlen_recursion(s + 1) + 1;
+		i +=  index(s + 1) + 1;
 	}
 	return (i);
+}
 
+/**
+ *is_palindrome- entry point
+ *@s:pow
+ * * Return: size of @s
+ */
+
+int is_palindrome(char *s)
+{
+	return (_square(s, index(*s) - 1, 0));
+}
