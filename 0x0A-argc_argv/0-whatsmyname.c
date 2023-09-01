91b7af6ff16c7c8c07d2b92eb93ebe5782d9c7a3
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-01 06:00:25 -0700

    hello

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * @argc:parameter
+ * @argv:array of string
+ * Return: 0 successful
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
