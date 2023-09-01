92556463ee5cfe709f80c9e252822dae612952f2
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-01 06:30:41 -0700

    hello

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main-entry point
  * @argc:parameter
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d\n", argv[1] + argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	else
 	{
