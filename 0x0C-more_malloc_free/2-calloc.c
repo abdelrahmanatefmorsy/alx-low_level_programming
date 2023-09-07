6167f5e2d9fa2415312f2977bb154826316b940b
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-07 06:13:09 -0700

    hello

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_calloc-entry point
+*@nmemb : pointer to first
+*@size: pointer to second string
+* Return: m successful
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *block;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	block = malloc(nmemb * size);
+	if (block != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+			block[i] = 0;
+		return (block);
+	}
+	else
+		return (NULL);
+}
