4c912b654264a632c8aa45a1455af1b7a0deedd2
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-11 06:11:20 -0700

    hello

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: modulas
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
