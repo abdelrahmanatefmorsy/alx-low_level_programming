47dd284a9a4b2cd5bee94dc7b890a71408028464
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-11 09:11:25 -0700

    hello

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+
+/**
+ * print_dog- entry point
+ *@d:pointer of struct
+ * * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+			printf("Name: (nil)");
+		if (d->age)
+		{
+			printf("Age: %ld\n", d->age);
+		}
+		else
+			printf("Age: (nil)");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)");
+	}
+}
