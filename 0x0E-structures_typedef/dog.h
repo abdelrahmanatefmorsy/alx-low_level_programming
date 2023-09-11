0f8b1022fe9bbaafe2727613754211c7df9df0b6
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-11 08:50:09 -0700

    hello

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_H
+#define _dog_H
+/**
+ * struct dog - dog
+ * @name:dog name
+ * @age: age of dog
+ * @owner:owner of dog
+ * Description : dog struct
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
