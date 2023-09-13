f3763de6d779d884589c8ccbeded5c7a564e2d22
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-13 07:14:35 -0700

    hello

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *print_name- entry point
+ * @name: saged
+ *@f:sagoda
+ * * Return: size of @s
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	return (f(name));
+}
