08d243fee25977e378a25d93d36e637a8ee4c40c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-13 07:25:07 -0700

    hello

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,10 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
-#endif 
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
