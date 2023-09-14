7a484425a5fea134014f7dac55899388a6a2c0e9
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-14 09:45:00 -0700

    hello

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list ptr;
 
-	if (n == 0)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(ptr, n);
 	while (i < n - 1)
 	{
