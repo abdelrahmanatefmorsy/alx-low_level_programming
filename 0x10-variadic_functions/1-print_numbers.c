da5f8931cc4214df906dc1a85ec57d433517c002
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-14 09:12:45 -0700

    hello

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - sum func
+*@n: arguments
+*@separator:char
+*Return: sum of all
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	va_list ptr;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ptr, n);
+	while (i < n - 1)
+	{
+		if (separator)
+		{
+			printf("%d%s", va_arg(ptr, int), separator);
+		}
+		else
+			printf("%d", va_arg(ptr, int));
+		i++;
+	}
+	printf("%d\n", va_arg(ptr, int));
+}
