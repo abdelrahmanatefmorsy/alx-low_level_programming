373394cbd3999cc62aec0d85e055855d8a05e9cd
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-21 11:54:29 -0700

    hello

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - print the list
+ * @head: the pointer which start
+ * Return: 0 success
+ */
+void free_list(list_t *head);
+{
+	list_t *count, h;
+
+	if (!head)
+		return;
+	count = head;
+	while (count != NULL)
+	{
+		h = count->next;
+		free(count->str);
+		free(count);
+		count = h;
+	}
+}
