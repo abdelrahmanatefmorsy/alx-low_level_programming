da0cdc41eca3bfa01c583a532b0cc855b88d91e9
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-21 04:13:07 -0700

    hello

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list- print the list
+ * @h: the pointer which start
+ * Return: 0 success
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *count = h;
+	int  i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (count != NULL)
+	{
+		if (!count->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", count->len, count->str);
+		count = count->next;
+		i++;
+	}
+	return (i);
+}
