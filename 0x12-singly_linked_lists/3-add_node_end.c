b9533e3bb22344336f13b9d42ca7b2f4dbf9a4ae
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-21 11:43:23 -0700

    hello

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node_end- print the list
+ * @head: the pointer which start
+ * @str: string
+ * Return: 0 success
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+	list_t *change = *head;
+
+	if (!ptr || !head)
+		return (NULL);
+	if (str)
+	{
+		ptr->str = strdup(str);
+		if (!ptr->str)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		ptr->len = strlen(ptr->str);
+	}
+	if (head)
+	{
+		while (change->next)
+			change = change->next;
+		change->next = ptr;
+	}
+	else
+		*head = ptr;
+	return (ptr);
+}
