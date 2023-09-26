c2b45625507e9a69be4881fa91a50d13f32a1613
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-26 01:59:40 -0700

    linked list pro

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint- print the list
+ * @h: the pointer which start
+ * Return: 0 success
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *count = h;
+	int  i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (count != NULL)
+	{
+		printf("%d", count->n);
+		count = count->next;
+		i++;
+	}
+	return (i);
+}
