d3ab55aa91f92cdfeee1278cfad06a708db35366
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-26 02:43:10 -0700

    linked list pro

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - give size of lists
+ * @head: the head pointer
+ * @n:the value of node
+ * Return: size of list
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *cnt;
+
+	ptr = *head;
+	cnt = malloc(sizeof(listint_t));
+	if (!ptr || !cnt)
+		return (NULL);
+	cnt->n = n;
+	cnt->next = NULL;
+	if (ptr == NULL)
+	{
+		*head = cnt;
+		return (cnt);
+	}
+	while (ptr->next)
+		ptr = ptr->next;
+	ptr->next = cnt;
+	return (cnt);
+}
