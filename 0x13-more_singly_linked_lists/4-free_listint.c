704593db9051d513850b1d9da094d9d1edd5b898
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-26 04:23:05 -0700

    linked list pro

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - print the list
+ * @head: the pointer which start
+ * Return: 0 success
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *count, *h;
+
+	if (!head)
+		return;
+	count = head;
+	while (count != NULL)
+	{
+		h = count->next;
+		free(count->str);
+		free(count);
+		count = h;
+	}
+}
