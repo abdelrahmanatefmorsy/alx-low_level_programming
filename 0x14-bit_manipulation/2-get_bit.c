5cf8891d4161e97863d1abe7c821ef5f9278fd26
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-28 06:03:38 -0700

    hello

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* get_bit - fuction to convert saged to binary
+* @n: string that will convert
+* @index: the insex we want
+* Return: saged
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long x = 1;
+
+	if (!n)
+		return (-1);
+	if ((n & (x << index)) > 0)
+		return (1);
+	else
+		return (0);
+}
