7d041c0969ad9dbd38805bd31fedea1863cdbd94
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-09-28 04:57:22 -0700

    hello

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a = 1, i = 0, sum = 0;
+	unsigned int a = 1, sum = 0;
+	int i = 0;
 
 	while (b[i])
 		i++;
 	i--;
-	for (i; i >= 0; i--)
+	while(i >= 0)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return (NULL);
+			return (0);
 		sum += (b[i] - '0') * a;
 		a *= 2;
+		i--;
 	}
 	return (sum);
 }
