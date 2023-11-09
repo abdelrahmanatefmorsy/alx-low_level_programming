509f1d5fc26c67e35ed705bf59c8f4ccb5eedd93
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-09 13:29:23 -0800

    dlinkedlist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - print the list
+ * @head: the pointer which start
+ * Return: 0 success
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *count, *h;
+
+	if (!head)
+		return;
+	count = head;
+	while (count != NULL)
+	{
+		h = count->next;
+		free(count->n);
+		free(count);
+		count = h;
+	}
+}
