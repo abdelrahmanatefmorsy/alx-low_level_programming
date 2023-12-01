8b9ed83064c0ac81dbc3af2e5f9a3eb333f7dbfc
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-12-01 09:21:33 -0800

    hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set -  set func
+ * @ht: hashtable struct
+ * @key: key
+ * @value: value
+ * Return: 1 or 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	hash_node_t *current;
+
+	unsigned int long index;
+
+	if (!key || !value || !ht)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (!current->value)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->value  = strdup(value);
+	if (!node->value)
+		return (0);
+	node->key = strdup(key);
+		if (!node->key)
+			return (0);
+	return (1);
+}
