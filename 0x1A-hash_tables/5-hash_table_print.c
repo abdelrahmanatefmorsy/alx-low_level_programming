b404df5213dc53ec7369b9410dd54dfc27b6f259
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-12-01 10:32:19 -0800

    hash tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,16 +11,16 @@ void hash_table_print(const hash_table_t *ht)
 	int is = 0;
 	hash_node_t *tmp;
 
-	if (!ht)
+	if (!ht || ht->array == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
-		while (tmp)
+		while (tmp != NULL)
 		{
 			if (is == 1)
-				printf(" , ");
+				printf(", ");
 			is = 1;
 			printf("'%s': '%s'", tmp->key, tmp->value);
 			tmp = tmp->next;
