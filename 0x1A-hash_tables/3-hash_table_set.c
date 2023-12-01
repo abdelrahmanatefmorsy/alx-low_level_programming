173d77d1293119bbe780eabd506a7cc577a3b73a
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-12-01 09:51:19 -0800

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,16 +36,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!node)
 		return (0);
 	node->value  = strdup(value);
-	if (!node->value)
+	if (node->value == NULL)
 	{
 		free(node);
 		return (0);
 	}
 	node->key = strdup(key);
-	if (!node->key)
+	if (node->key == NULL)
 	{
 		free(node->value), free(node);
 		return (0);
 	}
+	node->next = ht->array[index], ht->array[index] = node;
 	return (1);
 }
