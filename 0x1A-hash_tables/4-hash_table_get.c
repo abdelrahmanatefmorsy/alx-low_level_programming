628d0e38ed751650d61f744a93f78144bb858484
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-12-01 10:28:30 -0800

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,12 +11,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current;
 
-	unsigned int long index;
+	unsigned int  index;
 
-	index = key_index((unsigned char *)key, ht->size);
 	if (!ht || !key || !ht->size ||
 	 ht->array == NULL || strlen(key) == 0)
 		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current != NULL)
 	{
