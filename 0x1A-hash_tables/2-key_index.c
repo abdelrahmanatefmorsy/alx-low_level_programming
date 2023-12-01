eb632d70d3365a0a622cdb74eb08c63b07f7a9b9
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-12-01 08:17:42 -0800

    hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index- get index
+ * @key : string
+ * @size: size
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+	return (hash_djb2(key) % size);
+
+}
+
