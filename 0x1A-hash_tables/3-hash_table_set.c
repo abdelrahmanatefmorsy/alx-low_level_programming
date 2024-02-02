c884fcd85d009844f65e3fbd770b0739b6d1f07d
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-02 12:18:17 -0800

    hello

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,63 +1,38 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - function that set element and key in hash table
- * @ht: hash table that created
- * @key : key of value
- * @value : value
+ * hash_table_set -  set func
+ * @ht: hashtable struct
+ * @key: key
+ * @value: value
  * Return: 1 or 0
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *current;
-	int new;
-	unsigned int size, index;
+	hash_node_t *node, *current;
 
+	unsigned int long index;
 
-	size = ht->size;
-	if (!size || !key || !value)
+	if (!key || !value || !ht)
 		return (0);
-	index = key_index((unsigned char *)key, size);
-	if (!index)
-		return (0);
+	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current->value = malloc(strlen(value) + 1);
+			current->value = malloc(sizeof(char) * strlen(value) + 1);
 			if (current->value == NULL)
 				return (0);
 			current->value = strdup(value);
 			if (!current->value)
-			{
-				free(current->value);
 				return (0);
-			}
 			return (1);
 		}
 		current = current->next;
 	}
-	new = add(ht, key, value, index);
-	return (new);
-}
-
-/**
- * add- added node
- * @ht: hash table
- * @key : key of node
- * @value : new value will added
- * @index: the index
- * Return: 1 or 0
- */
-int add(hash_table_t *ht, const char *key, const char *value,
-		unsigned int index)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t ));
+	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 	node->value  = strdup(value);
