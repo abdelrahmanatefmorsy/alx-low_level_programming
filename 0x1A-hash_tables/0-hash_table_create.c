f3675f9b1ea2bddf04b1ad9bfe12a2c4b543254c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-02 07:44:19 -0800

    hello

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash.h"
+/**
+ * hash_table_create - function that create a hash table
+ * @size: size of hash table
+ * Return: hash table that will create
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *create;
+	unsigned int i;
+
+	create = malloc(sizeof(hash_table_t *));
+	if (!create)
+		return (NULL);
+	create->array = malloc(sizeof(hash_node_t *) * size);
+	if (!create->array)
+		return (NULL);
+	create->size = size;
+	for (i = 0; i < size; i++)
+	{
+		create->array[i] = NULL
+	}
+	return (create);
+}
