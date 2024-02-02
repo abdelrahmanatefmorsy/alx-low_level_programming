0b285940065880fd8d17623b50b6197ec47fdab8
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-02 09:28:41 -0800

    hello

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,3 +44,35 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = add(ht, key, value, index);
 	return (new);
 }
+
+/**
+ * add- added node
+ * @ht: hash table
+ * @key : key of node
+ * @value : new value will added
+ * @index: the index
+ * Return: 1 or 0
+ */
+int add(hash_table_t *ht, const char *key, const char *value,
+		unsigned int index)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t *));
+	if (!node)
+		return (0);
+	node->value  = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node->value), free(node);
+		return (0);
+	}
+	node->next = ht->array[index], ht->array[index] = node;
+	return (1);
+}
