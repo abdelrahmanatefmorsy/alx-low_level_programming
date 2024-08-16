e8ad9ef6f82346943a35d0f7a505422dcdcea21e
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-08-16 13:46:59 +0300

    hello

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+/**
+ * print_search- entry point
+ *@l: p1
+ *@r: p2
+ *@array: array
+ * * Return:void
+ */
+
+void print_search(size_t l, size_t r, int *array)
+{
+	size_t i = l;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		printf("%d", array[i]);
+		if (i != r)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+/**
+ * binary_search- entry point
+ *@array: p1
+ *@size: p2
+ *@value: p3
+ * * Return:void
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l  = 0;
+	size_t r = size - 1;
+	size_t mid;
+
+	if (size == 0)
+		return (-1);
+	print_search(l, r, array);
+		while (l <= r)
+		{
+			mid = (l + r) / 2;
+			if (array[mid] > value)
+			{
+				r = mid - 1;
+				print_search(l, r, array);
+			}
+			else if (array[mid] < value)
+			{
+				l = mid + 1;
+				print_search(l, r, array);
+			}
+			else
+			{
+				return (mid);
+			}
+		}
+	return (-1);
+}
