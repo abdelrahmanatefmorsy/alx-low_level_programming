27e2a2d7d5d1cb5647b39ba5e54b6d904bc4f5ff
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2025-09-09 12:14:18 +0300

    ADD 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int _strspn(char *s, char *accept)
+{
+    int vis[1000] ={0};
+    char *ptr;
+    int sum = 0 , j;
+    for (ptr = accept; *ptr != '\0'; ptr++)
+    {
+        j = *ptr;
+        vis[j] = 1;
+    }
+    for (ptr = s; *ptr != ' ' && *ptr != '\0'; ptr++)
+    {
+        j = *ptr;
+        if(vis[j])
+            sum++;
+    }
+    return sum;
+    
+    
+}   
